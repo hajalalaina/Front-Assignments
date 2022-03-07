58400eb8c40772cf68f1cad3dd555755de86d4fb
micbuffa <micbuffa@gmail.com>
2022-03-07 10:40:37 +0100

    On commence les directives

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'assignment-app';
+  titre = 'Application de gestion des assignments...';
+  
+  constructor() {
+  }
 }
---
+++ src/app/assignments/assignments.component.ts
@@ -0,0 +1,31 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-assignments',
+  templateUrl: './assignments.component.html',
+  styleUrls: ['./assignments.component.css'],
+})
+export class AssignmentsComponent implements OnInit {
+  assignments = [
+    {
+      nom: 'Devoir angular de Mr Buffa',
+      dateDeRendu: "2022-03-28",
+      rendu:false
+    },
+    {
+      nom: 'Devoir Oracle de Mr Mopolo',
+      dateDeRendu: "2022-01-22",
+      rendu:true
+    },
+    {
+      nom: 'Devoir Grails de Mr Galli',
+      dateDeRendu: "2022-04-01",
+      rendu:false
+    },
+];
+
+  constructor() {}
+
+  // appelé après le constructeur et AVANT l'affichage du composant
+  ngOnInit(): void {}
+}
