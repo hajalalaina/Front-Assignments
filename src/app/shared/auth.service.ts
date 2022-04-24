286ebd714f71eb1a74e599d3c698c271154c5dbf
hajalalaina <rahajalalaina@yahoo.fr>
2022-04-24 14:32:36 +0300

    connect api->login

--- src/app/shared/auth.service.ts
+++ src/app/shared/auth.service.ts
@@ -1,16 +1,29 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { User } from '../login/user.model';
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class AuthService {
   loggedIn = false;
+  url = 'http://localhost:8010/api/users';
+  //POST  nom: mdp:
+  //body = {nom:input,mdp:input}
+  constructor(private http: HttpClient) {}
 
-  logIn(login:string, password:string) {
+  logIn(login: string , password: string) {
     // normalement il faudrait envoyer une requête sur un web service, passer le login et le password
     // et recevoir un token d'authentification, etc. etc.
 
     // pour le moment, si on appelle cette méthode, on ne vérifie rien et on se loggue
-    this.loggedIn = true;
+    const body = {
+      nom: login,
+      mdp: password,
+    };
+
+    return this.http.post<User>(this.url, body);
+
+    // this.loggedIn = true;
   }
 
   logOut() {
@@ -26,6 +39,4 @@ export class AuthService {
   }
 
   // isAdmin().then(admin => { if(admin) { console.log("L'utilisateur est administrateur"); }})
-
-  constructor() { }
 }
