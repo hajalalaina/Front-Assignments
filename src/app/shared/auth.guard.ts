0d8f50b0b225e512f33f642b6e414cd327cc83e2
hajalalaina <rahajalalaina@yahoo.fr>
2022-04-28 09:27:20 +0300

    added admin guard

--- src/app/shared/auth.guard.ts
+++ src/app/shared/auth.guard.ts
@@ -24,45 +24,14 @@ export class AuthGuard implements CanActivate {
     | Promise<boolean | UrlTree>
     | boolean
     | UrlTree {
-    if (!localStorage.getItem('token')) {
-      this.router.navigate(['/login']);
-      return false;
-    } else {
-      //check if token not expired
-      const token = localStorage.getItem('token');
-      if (token) {
-        const tokenValue: any = jwt_decode(token);
-        let expired: boolean = true;
-        expired = new Date(tokenValue.exp * 1000) < new Date();
-        console.log(
-          'expired?',
-          expired,
-          ' at ',
-          new Date(tokenValue.exp * 1000)
-        );
-        if (expired) {
-          this.router.navigate(['/login']);
-          localStorage.removeItem('token');
-          return false;
-        }
-      }
-    }
-    return true;
-
-    // si renvoie true ça dit que les routes associées à ce gardien sont navigables
-    return this.authService.isAdmin().then((admin): boolean => {
-      //console.log("admin = " + admin + " type : " + (typeof admin))
-      if (admin) {
-        console.log('GARDIEN autorise la navigation, vous êtes bien un admin');
+    const token = localStorage.getItem('token');
+    if (token) {
+      const tokenValue: any = jwt_decode(token);
+      if (tokenValue.user.role == 1) {
         return true;
-      } else {
-        // si pas admin on force la navigation vers la page d'accueil
-        console.log(
-          "GARDIEN n'autorise pas la navigation, vous n'êtes pas admin"
-        );
-        this.router.navigate(['/home']);
-        return false;
       }
-    });
+      console.log('guard auth', tokenValue.user);
+    }
+    return false;
   }
 }
---
+++ src/app/shared/login.guard.ts
@@ -0,0 +1,53 @@
+import { Injectable } from '@angular/core';
+import {
+  ActivatedRouteSnapshot,
+  CanActivate,
+  Router,
+  RouterStateSnapshot,
+  UrlTree,
+} from '@angular/router';
+import jwt_decode from 'jwt-decode';
+import { Observable } from 'rxjs';
+import { AuthService } from './auth.service';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class LoginGuard implements CanActivate {
+  constructor(private authService: AuthService, private router: Router) {}
+
+  canActivate(
+    route: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot
+  ):
+    | Observable<boolean | UrlTree>
+    | Promise<boolean | UrlTree>
+    | boolean
+    | UrlTree {
+    if (!localStorage.getItem('token')) {
+      this.router.navigate(['/login']);
+      return false;
+    } else {
+      //check if token is not expired
+      const token = localStorage.getItem('token');
+      if (token) {
+        const tokenValue: any = jwt_decode(token);
+        let expired: boolean = true;
+        expired = new Date(tokenValue.exp * 1000) < new Date();
+        console.log(tokenValue);
+        console.log(
+          'expired?',
+          expired,
+          ' at ',
+          new Date(tokenValue.exp * 1000)
+        );
+        if (expired) {
+          this.router.navigate(['/login']);
+          localStorage.removeItem('token');
+          return false;
+        }
+      }
+    }
+    return true;
+  }
+}
